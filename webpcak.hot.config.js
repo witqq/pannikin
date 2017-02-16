f2cdbb0669ce3f5875c6fabf38ef2b84fade41b8
whitemq <golyi91@mail.ru>
2017-02-16 23:43:51 +0500

    initial

--- webpcak.hot.config.js
+++ webpcak.hot.config.js
@@ -13,7 +13,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: "/dist/"
+        publicPath: "dist/"
     },
 
     devtool: "eval",
